e5bb2d2b5b72be1e117500c9fc081db3c346dad7
Daniil56 <emelyanov.daniil93@gmail.com>
2017-04-02 19:21:20 +0500

    task 41123

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,17 +1,19 @@
 package ru.job4j;
 
 /**
- *Class Calculate решение задачи части 001 урок 1.
- *@author demelyanov
- *@since 02.04.2017
+ * Calculate решение задачи части 001 урок 1.
+ *
+ * @author demelyanov
+ * @version
+ * @since 02.04.2017
  */
 
-public class Calculate{
-	public static void main(String[] args){
+public class Calculate {
+	/**
+	* Main.
+	* @param args - args.
+	*/
+	public static void main(String[] args) {
 		System.out.println("Hello, World");
 	}
 }
-/**
- * Конструктор, вывод строки в консоль.
- * @param arg - arg
- */

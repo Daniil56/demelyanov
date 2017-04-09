e7f470af3e46842a5a359c201b16a149570b72fd
Daniil56 <emelyanov.daniil93@gmail.com>
2017-04-09 12:35:30 +0500

    189test

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,9 +1,20 @@
 package ru.job4j.max;
+/**
+ * Max.
+ *
+ * @author Daniil Emelyanov.
+ * @version %Id%
+ * @since 0.1
+ */
 
 public class Max {
-	public int max(int first, int second) { 
+	/**...
+	* Max in two
+	* @param first - first value
+	* @param second - second value
+	* @return max among
+	*/
+public int max(int first, int second) {
 	return first > second ? first : second;
 	 }
 }
-	
-

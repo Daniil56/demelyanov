a1d838181c9b1d6c9e1853258b4b815f0cd79059
Daniil56 <emelyanov.daniil93@gmail.com>
2017-04-09 12:08:34 +0500

    189

---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**...
+ * Test.
+ * @author Daniil Emelyanov
+ * @version %Id%
+ * @since 0.1
+ */
+public class MaxTest {
+	/**...
+	* Test max with two args.
+	*/
+	@Test
+	public void whenMaxWhithTwoArgs() {
+		Max maxvalue = new Max();
+		assertThat(maxvalue.max(3, 2), is(3));
+	 }
+}

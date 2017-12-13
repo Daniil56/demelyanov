065379b1d1fc78232a284063f844607176e26fbe
Daniil56 <emelyanov.daniil93@gmail.com>
2017-12-13 17:35:25 +0500

    %check%

---
+++ chapter_001/src/main/java/ru/job4j/condition/abc.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition
+
+public class Abc {
+	private Point a;
+	private Point b;
+	private Point c;
+	
+	public Abc(Point a, Point b, Point c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public double area() {
+		
+	}
+}
